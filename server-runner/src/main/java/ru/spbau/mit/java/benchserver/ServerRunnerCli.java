583f2c16220d4e7df2813a2e3725a1ae3027aa56
Egor Gorbunov <egor-mailbox@ya.ru>
2017-02-16 12:57:50 +0300

    Server cli input error handling

--- server-runner/src/main/java/ru/spbau/mit/java/benchserver/ServerRunnerCli.java
+++ server-runner/src/main/java/ru/spbau/mit/java/benchserver/ServerRunnerCli.java
@@ -9,20 +9,47 @@ import java.io.InputStreamReader;
 
 @Slf4j
 public class ServerRunnerCli {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("USAGE: java -jar server.jar [PORT]");
+            System.exit(1);
         }
-        ServerRunner server = new ServerRunner(Integer.valueOf(args[0]));
+
+        int port = 0;
+        try {
+            port = Integer.valueOf(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("ERROR: Bad port. Must be integer");
+            System.exit(1);
+        }
+
+        ServerRunner server = null;
+        try {
+            server = new ServerRunner(port);
+        } catch (IOException e) {
+            System.err.println("Error starting server: " + e);
+            System.exit(1);
+        }
 
         server.start();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             System.out.print("CMD >> ");
-            String str = br.readLine();
+            String str = null;
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                System.err.println("Error reading line: " + e);
+                System.exit(1);
+            }
             if (str == null || str.equals("stop")) {
-                server.stop();
+                try {
+                    server.stop();
+                } catch (IOException e) {
+                    System.err.println("Error stopping server: " + e);
+                    System.exit(1);
+                }
                 break;
             }
         }
